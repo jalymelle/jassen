b9fcf1a3473609d6b3cb6f2dfdf5f20ce6a0c2bc
megan320 <=>
2021-06-25 10:14:59 +0200

    second commit

--- src/board/models.py
+++ src/board/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Jassarten(models.Model):
+    types = ['eichle', 'rose', 'schilte', 'schälle', 'obeabe', 'uneufe', 'miser',
+    'wahl', 'slalom', '5/4']
+    for t in types:
+        t = models.IntegerField(name=t, max_length=2)
+        
+        
