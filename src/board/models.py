9dd92bca42f4c3c764df695fe176864e989ef0b3
megan320 <jalymelle@prontonmail.com>
2021-06-25 10:19:34 +0200

    second again

--- src/board/models.py
+++ src/board/models.py
@@ -6,6 +6,6 @@ class Jassarten(models.Model):
     types = ['eichle', 'rose', 'schilte', 'schälle', 'obeabe', 'uneufe', 'miser',
     'wahl', 'slalom', '5/4']
     for t in types:
-        t = models.IntegerField(name=t, max_length=2)
-        
+        t = models.IntegerField(name=t)
+
         
