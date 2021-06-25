5b33f807916eb81857956b3b189435007261fcd1
megan320 <jalymelle@protonmail.com>
2021-06-25 13:59:22 +0200

    Model for Points

--- src/board/models.py
+++ src/board/models.py
@@ -2,10 +2,14 @@ from django.db import models
 
 # Create your models here.
 
-class Jassarten(models.Model):
-    types = ['eichle', 'rose', 'schilte', 'schälle', 'obeabe', 'uneufe', 'miser',
+class Jas(models.Model):
+    pass
+
+
+labels = ['eichle', 'rose', 'schilte', 'schälle', 'obeabe', 'uneufe', 'miser',
     'wahl', 'slalom', '5/4']
-    for t in types:
-        t = models.IntegerField(name=t)
+for label in labels:
+    Jas.add_to_class(label + '_1', models.IntegerField(default=0))
+    Jas.add_to_class(label + '_2', models.IntegerField(default=0))
 
         
