995298ee82644a2ce4ae8086e6b72b8d3ae20ca7
megan320 <jalymelle@protonmail.com>
2021-06-30 10:36:32 +0200

    updated model

--- src/board/models.py
+++ src/board/models.py
@@ -6,8 +6,7 @@ class Jas(models.Model):
     team_name = models.CharField(max_length=20, null=False, unique=True)
 
 
-labels = ['eichle', 'rose', 'schilte', 'schälle', 'obeabe', 'uneufe', 'miser',
-    'wahl', 'slalom', '5/4']
+labels = ['Ei', 'Ro', 'Si', 'Se', 'Mi', 'Ob', 'Un', 'Sl', '4/5', '?', '3/3', 'Ro12']
 for label in labels:
     Jas.add_to_class(label, models.IntegerField(default=0))
 
