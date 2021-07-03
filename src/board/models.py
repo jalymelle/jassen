1540d3f1347c49c86c154c217818304b42551b96
megan320 <jalymelle@protonmail.com>
2021-07-03 09:18:43 +0200

    layout part 1

--- src/board/models.py
+++ src/board/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 # Create your models here.
-
 class JassTeam(models.Model):
     team_name = models.CharField(max_length=20, null=False, unique=True)
 
@@ -9,5 +8,7 @@ class JassTeam(models.Model):
 labels = ['Ei', 'Ro', 'Si', 'Se', 'Mi', 'Ob', 'Un', 'Sl', '4/5', '?', '3/3', 'Ro12']
 for label in labels:
     JassTeam.add_to_class(label, models.IntegerField(default=0))
+
+
 
         
--- src/board/views.py
+++ src/board/views.py
@@ -2,11 +2,40 @@ from django.shortcuts import render
 from .models import JassTeam
 
 # Create your views here.
+#def start(request):
+    #return render(request, 'board/start.html', {})
+
+#def start(request):
+    #return render('board/start.html', {'output': output})
+
+
 def start(request):
-    return render(request, 'board/start.html', {})
+    model = JassTeam
+    field_names = [f.name for f in model._meta.get_fields()]
+    data = [[getattr(ins, name) for name in field_names]
+            for ins in JassTeam.objects.prefetch_related().all()]
+    return render(request, 'board/start.html', {'field_names': field_names, 'data': data})
 
 
 def board(request):
-    Jassarten = JassTeam.objects.all()
-    print(Jassarten)
-    return render(request, 'board/board.html', {'Jassarten': Jassarten})
+    team1 = JassTeam.objects.get(id=1)
+    team2 = JassTeam.objects.get(id=2)
+
+    fields = JassTeam._meta.get_fields()
+    fields = fields[1:]
+    names = [field.name for field in fields]
+    fields_1 = []
+    fields_2 = []
+
+    for name in names:
+        field_object = JassTeam._meta.get_field(name)
+        fields_1.append(field_object.value_from_object(team1))
+        fields_2.append(field_object.value_from_object(team2))
+    
+    
+    context = {
+        'names': names, 
+        'team1': fields_1,
+        'team2': team2}
+        
+    return render(request, 'board/board.html', context)
